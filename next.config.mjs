5e5b7f3272e4c3710fd26f27710ec06688322207
ManjishPC <manjishupadhaya@gmail.com>
2026-05-22 23:40:52 +0545

    fix: enforce output file tracing for sparticuz chromium binaries

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   serverExternalPackages: ['@sparticuz/chromium', 'playwright-core', 'puppeteer-core'],
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./node_modules/@sparticuz/chromium/bin/**/*"],
+  },
 };
 
 export default nextConfig;
